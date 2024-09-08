7255e67b12aaaeea3673a47a6803f3de023cee8a
Ninaswiftie09 <naj231088@uvg.edu.gt>
2024-09-07 19:28:16 -0600

    SplashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,24 +29,19 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
     buildFeatures {
         compose = true
     }
-
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -64,12 +59,14 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    // Testing dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
+
 }
--- app/src/main/java/com/example/desing/MainActivity.kt
+++ app/src/main/java/com/example/desing/MainActivity.kt
@@ -3,20 +3,22 @@ package com.example.desing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.desing.ui.theme.DesingTheme
-import androidx.compose.ui.Alignment
-
+import com.example.desing.R 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,13 +48,13 @@ fun SplashScreen() {
         contentAlignment = Alignment.Center
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            // Espacio para el icono
-            Box(
-                modifier = Modifier
-                    .size(120.dp)
-                    .background(Color.Gray, CircleShape)
+
+            Image(
+                painter = painterResource(id = R.drawable.sombrero_chef),
+                contentDescription = "Chef Hat",
+                modifier = Modifier.size(120.dp)
             )
-            Spacer(modifier = Modifier.height(16.dp)) // Espacio entre el icono y el texto
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "Chef Recipes",
                 fontSize = 32.sp,
